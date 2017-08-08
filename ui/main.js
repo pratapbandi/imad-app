b9206bebe2a671c38882b2c1781826bb936c0dba
pratapbandi <30556431+pratapbandi@users.noreply.github.com>
2017-08-08 22:06:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,1 @@
-console.log('Loaded!');
-//change text
-var  element = document.getElementById('main-text');
-element.innerHTML = 'New value by Patap.';
-
-//moving image
-var img = document.getElementById('madi');
-var marginleft = 0;
-function moveRight(){
-    marginleft = marginleft + 1;
-    img.style.marginleft = marginleft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-};
+//console code
