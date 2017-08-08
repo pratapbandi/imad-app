4d551d42ea62de9236c3eca6a513a433d4cd701a
pratapbandi <30556431+pratapbandi@users.noreply.github.com>
2017-08-08 22:30:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function (){
          if(request.status === 200){
              var counter = request.responseText;
              var span = document.getElementById('count');
-             span.innerHtml = counter.toString();
+             span.innerHTML = counter.toString();
          }
      }  
    };
